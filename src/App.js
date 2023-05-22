2f97dd85a79286ae128729647eaf971477b4340c
Zak <izak@moonsite.co.il>
2023-05-23 01:23:15 +0300

    initial layout and router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import Home from './pages/home/Home';
+import Reservations from './pages/reservation/Reservations';
+
+import {Route, Routes, BrowserRouter} from "react-router-dom";
+
 
 function App() {
   return (
     <div>
-      homepage
+      <Header />
+      <BrowserRouter>
+      <Routes>
+        <Route path = "/" element = {<Home />}/>
+        <Route path = "/reservations" element = {<Reservations />}/>
+      </Routes>
+      </BrowserRouter>
+      <Footer />
     </div>
   );
 }
