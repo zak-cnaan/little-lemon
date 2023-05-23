bfca5ac9bfad7f1138936f2146b5bfdadaadd3f0
Zak <izak@moonsite.co.il>
2023-05-23 08:21:15 +0300

    header layout:

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,38 +1,40 @@
 import Logo from "../../assets/images/logo.jpg";
-import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
+import { Navbar, Nav, Container } from "react-bootstrap";
+
 const Header = () => {
-  const [click, setClick] = useState(false);
-  const handleClick = () => setClick(!click);
 
   return (
-    <div as="header">
-        <button  type="button" className="btn btn-primary">asdf</button>
-      <div maxW={"container.xl"}>
-        <div px={0} py={4} justifyContent="space-between" alignItems="center">
-          <NavLink to="/">
-            {<img className="logo-img" src={Logo} alt="Little Lemon logo" />}
-          </NavLink>
-          <nav className="nav">
-            <ul className={click ? "nav-menu active" : "nav-menu"}>
-              <li>
-                <NavLink to="/">Home</NavLink>
-              </li>
-              <li>
-                <NavLink to="#">About</NavLink>
-              </li>
-              <li>
-                <NavLink to="#">Menu</NavLink>
-              </li>
-              <li>
-                <NavLink to="/reservations">Reservations</NavLink>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-    </div>
+    <>
+      <Navbar bg="light" expand="lg" as='header'>
+        <Container>
+          <Navbar.Brand as={NavLink} to="/" className="active">
+            <img className="logo-img" src={Logo} alt="Little Lemon logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link as={NavLink} to="/" className="active">
+                Home
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="/#About" className="active">
+                About
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="/#About" className="active">
+                About
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="/#Tetimonials" className="active">
+                Tetimonials
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="/reservations" className="active">
+                Reservations
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
   );
 };
 
