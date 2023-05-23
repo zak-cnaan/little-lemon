8099824ff6d6e832f3d443b09b279fe4831a4f4a
Zak <izak@moonsite.co.il>
2023-05-24 02:29:03 +0300

    sticky header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,40 @@
 import Logo from "../../assets/images/logo.jpg";
 import { NavLink } from "react-router-dom";
+import { useEffect, useRef } from "react";
 
 import { Navbar, Nav, Container } from "react-bootstrap";
 
 const Header = () => {
 
+  const headerRef = useRef(null);
+
+
+    useEffect(() => {
+        let prevScrollPos = window.scrollY;
+    
+        const handleScroll = () => {
+          const currentScrollPos = window.scrollY;
+          const headerElement = headerRef.current;
+          if (!headerElement) {
+            return;
+          }
+          if (prevScrollPos > currentScrollPos) {
+            headerElement.style.transform = "translateY(0)";
+          } else {
+            headerElement.style.transform = "translateY(-200px)";
+          }
+          prevScrollPos = currentScrollPos;
+        };
+        window.addEventListener("scroll", handleScroll);
+    
+        return () => {
+          window.removeEventListener("scroll", handleScroll);
+        };
+      }, []);
+
   return (
     <>
-      <Navbar bg="light" expand="lg" as='header'>
+      <Navbar bg="light" expand="lg" as='header' ref={headerRef}>
         <Container>
           <Navbar.Brand as={NavLink} to="/" className="active">
             <img className="logo-img" src={Logo} alt="Little Lemon logo" />
